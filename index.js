5703b97ba7b864c8d92b3bfc175e00417f93aac0
Roman <rpol123453@gmail.com>
2022-04-13 14:22:45 +0300

    Add router to index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const cors = require('cors');
+const router = require('./routes');
 
 const PORT = 5000;
 const app = express();
 app.use(express.json());
 app.use(cors({ origin: 'https://romanpolianski.github.io' }));
+app.use('/api', router);
 
 const start = async () => {
     try {
