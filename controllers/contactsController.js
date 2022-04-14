decb6156c9ca5d62aee493609d15d69410ffd013
Roman <rpol123453@gmail.com>
2022-04-14 15:27:28 +0300

    Add getTasks

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,27 +1,33 @@
+/* eslint-disable no-unused-vars */
+const { sequelize, contacts } = require('../models')
+
 class ContactsController {
-    getContacts(req, res) {
-      try {
-        console.log('Get');
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
+    async getContacts(req, res) {
+        try {
+            const contact = await contacts.findAll({
+                include: ['tasks'],
+            });
+            res.json(contact);
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
-  
+
     createContact(req, res) {
-      try {
-        console.log('Create');
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
+        try {
+            console.log('Create');
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
-  
+
     updateContact(req, res) {
-      try {
-        console.log('Update');
-      } catch (error) {
-        res.status(500).json({ error: error.message });
-      }
+        try {
+            console.log('Update');
+        } catch (error) {
+            res.status(500).json({ error: error.message });
+        }
     }
-  }
-  
-  module.exports = new ContactsController();
+}
+
+module.exports = new ContactsController();
